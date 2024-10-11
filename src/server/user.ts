68c4cbb4d25e7c2e5800f24884910f2eacf3166e
kweeyf <keane.wee.2024@computing.smu.edu.sg>
2024-10-11 11:59:13 +0800

    init

--- src/server/user.ts
+++ src/server/user.ts
@@ -1,7 +1,7 @@
-import {drizzle} from "drizzle-orm/vercel-postgres";
-import {db} from "../../db";
+// import {drizzle} from "drizzle-orm/vercel-postgres";
+// import {db} from "../../db";
 
-export default async function inserOneUser(name: string, isDrugDealer:boolean) {
-    await db.insert.(users).values({name: name, isDrugDealer: isDrugDealer});
-}
+// export default async function inserOneUser(name: string, isDrugDealer:boolean) {
+//     await db.insert.(users).values({name: name, isDrugDealer: isDrugDealer});
+// }
 
